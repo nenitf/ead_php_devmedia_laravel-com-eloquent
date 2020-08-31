167a82c91a4afb67195dfa8a3dad33552c3d5ce6
Felipe Silva <felipe.dacs@gmail.com>
2020-08-31 01:21:16 -0300

    :tv: add aula 9

---
+++ resources/lang/pt_BR/auth.php
@@ -0,0 +1,20 @@
+<?php
+# THANKS: https://raw.githubusercontent.com/lucascudo/laravel-pt-BR-localization/master/src/pt_BR/auth.php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Essas credenciais não foram encontradas em nossos registros.',
+    'throttle' => 'Muitas tentativas de login. Tente novamente em :seconds segundos.',
+
+];
