1b011ae936c1bde9747756f0321da22528a4327b
Felipe Silva <felipe.dacs@gmail.com>
2020-08-30 23:51:06 -0300

    :tv: add aula 4

---
+++ resources/views/shared/base.blade.php
@@ -0,0 +1,44 @@
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Administração de Imóveis</title>
+
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <style>
+            body { padding-top: 70px; }
+        </style>
+    </head>
+
+    <body>
+
+        <nav class="navbar navbar-default navbar-fixed-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">Início</a>
+                </div>
+                <div id="navbar" class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav">
+                        <li><a href="#">Todos</a></li>
+                        <li><a href="#">Apartamentos</a></li>
+                        <li><a href="#">Casas</a></li>
+                        <li><a href="#">Kitnet</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <div class="container">
+            @yield('content')
+        </div>
+    </body>
+</html>
